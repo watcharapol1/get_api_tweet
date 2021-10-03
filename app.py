de55be95fee2d5fc5a110133c51c262a7d20b0e1
watcharapol1 <91687237+watcharapol1@users.noreply.github.com>
2021-10-03 23:33:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ app = Flask(__name__)
 def hash():
 	if request.method=='POST':
 		hashtag = request.form['hashtag']
-		h = tweepy.Cursor(api.search, q = hashtag, lang = "th").items()
-		return render_template('hash.html', h = h, hashtag = hashtag)
+# 		h = tweepy.Cursor(api.search, q = hashtag, lang = "th").items()
+# 		return render_template('hash.html', h = h, hashtag = hashtag)
 
 	return render_template('map.html')
 
